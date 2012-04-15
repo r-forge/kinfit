1c54c8d5b8d61b6a99b3df03e130e722115f8848
jranke <jranke@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-04-15 22:49:57 +0000

    - Fixed the SFORB coefficient matrix, at least for the case where parent is SFORB - This version works but is slower and less stable than mkin 0.8-11 - mkinplot is not adapted to this version
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/kinfit@25 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/test.R
+++ pkg/test.R
@@ -1,4 +1,5 @@
 install.packages("mkin")
+library(colorout)
 library(mkin)
 # {{{ Source new versions of mkin functions
 source("mkin/R/ilr.R")
@@ -6,9 +7,10 @@ source("mkin/R/mkinpredict.R")
 source("mkin/R/transform_odeparms.R")
 source("mkin/R/mkinmod.R")
 source("mkin/R/mkinfit.R")
+source("mkin/R/mkinplot.R")
 # }}}
 # {{{ Delete new versions of mkin functions
-rm(mkinmod, mkinfit, summary.mkinfit, print.summary.mkinfit)
+rm(mkinmod, mkinfit, summary.mkinfit, print.summary.mkinfit, mkinplot)
 rm(ilr, invilr, transform_odeparms, backtransform_odeparms)
 # }}}
 detach(package:mkin)
@@ -23,6 +25,7 @@ model <- mkinmod(
     A2 = list(type = "SFO"))
 
 fit <- mkinfit(model, data, plot=TRUE)
+fit <- mkinfit(model, data, eigen=TRUE, plot=TRUE)
 summary(fit)#}}}
 model2 <- mkinmod(#{{{
     parent = list(type = "FOMC", to = c("A1", "B1", "C1"), sink = FALSE),
@@ -41,14 +44,21 @@ model3 <- mkinmod(#{{{
 fit3 <- mkinfit(model3, data, plot=FALSE)
 summary(fit3)#}}}
 
+SFO <- mkinmod(parent = list(type = "SFO"))
+DFOP <- mkinmod(parent = list(type = "DFOP"))
+SFORB <- mkinmod(parent = list(type = "SFORB"))
 SFO_SFO <- mkinmod(parent = list(type = "SFO", to = "m1"),
 	           m1 = list(type = "SFO"))
 FOMC_SFO <- mkinmod(parent = list(type = "FOMC", to = "m1"),
 	           m1 = list(type = "SFO"))
-SFORB_SFO <- mkinmod(parent = list(type = "SFORB", to = "m1"),
-	           m1 = list(type = "SFO"))
 DFOP_SFO <- mkinmod(parent = list(type = "DFOP", to = "m1"),
 	           m1 = list(type = "SFO"))
+SFORB_SFO <- mkinmod(parent = list(type = "SFORB", to = "m1"),
+	           m1 = list(type = "SFO"))
+system.time(fit.SFORB <- mkinfit(SFORB_SFO, d2, 
+  parms.ini = f.SFORB$parms.all,
+  plot=TRUE))
+summary(fit.SFORB, data=FALSE)
 
 debug(transform_odeparms)
 undebug(transform_odeparms)
@@ -57,19 +67,23 @@ d2 <- rbind(FOCUS_2006_C,
        name = "m1", 
        time = FOCUS_2006_C$time,
        value = c(0, 20, 40, 45, 48, 49, 48, 46, 45)))
-system.time(fit.SFORB <- mkinfit(SFORB_SFO, d2, 
- parms.ini = c(
-   f_parent_to_m1 = 0.67, k_m1 = 0.002,
-   k_parent_free_bound = 0.05, k_parent_bound_free = 0.03),
- plot=TRUE))
-summary(fit.SFORB, data=FALSE)
-system.time(fit.SFORB <- mkinfit(SFORB_SFO, d2, plot=TRUE))
+system.time(fit.SFO <- mkinfit(SFO_SFO, d2, plot=TRUE))
 system.time(fit.FOMC <- mkinfit(FOMC_SFO, d2, plot=TRUE))
+f.DFOP <- mkinfit(DFOP, d2)
 system.time(fit.DFOP <- mkinfit(DFOP_SFO, d2, 
-  parms.ini = c(k1 = 0.5, k2 = 0.02, g = 0.8, 
-		k_m1 = 0.002, f_parent_to_m1 = 0.66),
+  parms.ini = f.DFOP$parms.all,
   plot=TRUE))
-summary(fit.DFOP, data=FALSE)
+f.SFORB <- mkinfit(SFORB, d2, plot=TRUE)
+summary(f.SFORB, data = FALSE)
+system.time(fit.SFORB <- mkinfit(SFORB_SFO, d2, 
+  parms.ini = f.SFORB$parms.all,
+  plot=TRUE))
+summary(fit.SFORB, data=FALSE)
+system.time(fit.SFORB <- mkinfit(SFORB_SFO, d2, 
+  parms.ini = f.SFORB$parms.all,
+  eigen=TRUE,
+  plot=TRUE))
+summary(fit.SFORB, data=FALSE)
 
 system.time(fit.SFO <- mkinfit(SFO_SFO, FOCUS_2006_D, plot=TRUE))
 system.time(fit.SFORB <- mkinfit(SFORB_SFO, FOCUS_2006_D, plot=TRUE))
