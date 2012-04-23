6720ed9dbee6b1bc274d68df32bbf2616c93fd23
jranke (Johannes Ranke) <jranke@uni-bremen.de>
2012-04-23 23:08:19 +0000

    - Fixed a couple of things - Now the eigenvalue based solutions are nicely consistent with the deSolve solutions, if enough   output times are specified (100, sometimes more are needed, see test.R) - Workaround for invilr not to produce NaN values so often - Still a lot to do (see TODO)
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/kinfit@30 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/test.R
+++ pkg/test.R
@@ -8,70 +8,55 @@ source("mkin/R/mkinmod.R")
 source("mkin/R/mkinfit.R")
 source("mkin/R/mkinplot.R")
 # }}}
-# {{{ Delete new versions of mkin functions
-rm(mkinmod, mkinfit, summary.mkinfit, print.summary.mkinfit, mkinplot)
-rm(ilr, invilr, transform_odeparms, backtransform_odeparms)
-# }}}
-detach(package:mkin)
-detach(package:kinfit)
 
-data <- mkin_wide_to_long(schaefer07_complex_case, time = "time")#{{{
-model <- mkinmod(
-    parent = list(type = "SFO", to = c("A1", "B1", "C1"), sink = FALSE),
-    A1 = list(type = "SFO", to = "A2"),
-    B1 = list(type = "SFO"),
-    C1 = list(type = "SFO"),
-    A2 = list(type = "SFO"))
-model <- mkinmod(
-    parent = list(type = "SFO", to = c("A1", "B1", "C1"), sink = FALSE),
-    A1 = list(type = "SFO", to = "A2"),
-    B1 = list(type = "SFO"),
-    C1 = list(type = "SFO"),
-    A2 = list(type = "SFO"), use_of_ff = "max")
-model
-
-fit <- mkinfit(model, data, plot=TRUE)
-fit <- mkinfit(model, data, eigen=TRUE, plot=TRUE)
-summary(fit, data=FALSE)#}}}
-model2 <- mkinmod(#{{{
-    parent = list(type = "FOMC", to = c("A1", "B1", "C1"), sink = FALSE),
-    A1 = list(type = "SFO", to = "A2"),
-    B1 = list(type = "SFO"),
-    C1 = list(type = "SFO"),
-    A2 = list(type = "SFO"))
-fit2 <- mkinfit(model2, data, plot=TRUE, parms.ini = c(f_A1_to_A2 = 0.3))
-summary(fit2)#}}}
-model3 <- mkinmod(#{{{
-    parent = list(type = "SFORB", to = c("A1", "B1", "C1"), sink = FALSE),
-    A1 = list(type = "SFO", to = "A2"),
-    B1 = list(type = "SFO"),
-    C1 = list(type = "SFO"),
-    A2 = list(type = "SFO"))
-fit3 <- mkinfit(model3, data, plot=FALSE)
-summary(fit3)#}}}
-
-debug(mkinmod)
-
-test <- mkinmod(parent = list(type = "SFO", 
-
-SFO <- mkinmod(parent = list(type = "SFO"))
+# {{{ Parent only models, for one degradation data series named "parent"
+SFO.1 <- mkinmod(parent = list(type = "SFO"))
+SFO.2 <- mkinmod(parent = list(type = "SFO"), use_of_ff = "max")
+mkinpredict(SFO.1, c(k_parent_sink = 0.1), c(parent = 100), 1:100, solution_type = "analytical")
+mkinpredict(SFO.2, c(k_parent = 0.1), c(parent = 100), 1:100, solution_type = "analytical")
+FOMC <- mkinmod(parent = list(type = "FOMC"))
+HS <- mkinmod(parent = list(type = "HS"))
 DFOP <- mkinmod(parent = list(type = "DFOP"))
 SFORB <- mkinmod(parent = list(type = "SFORB"))
-# Test different types of model specification and solution
+# }}}
+
+# Dataset d2 {{{
+d2 <- rbind(FOCUS_2006_C, 
+    data.frame(
+       name = "m1", 
+       time = FOCUS_2006_C$time,
+       value = c(0, 20, 40, 45, 48, 49, 48, 46, 45)))
+# }}}
+
+# Check deSolve solution versus analytical for parent only case {{{
+ot = seq(0, 100, by = 1)
+print(mkinpredict(SFO, c(k_parent_sink = 0.1),
+	    c(parent = 100), ot, solution_type = "deSolve")[101,])
+print(mkinpredict(SFO, c(k_parent = 0.1),
+	    c(parent = 100), ot, solution_type = "analytical")[101,])
+# }}}
+
+# Some code useful for debugging {{{
+as.list(body(mkinfit))
+trace("mkinfit", quote(browser(skipCalls=4)), at = 55)
+untrace("mkinfit")
+# }}}
+
+# Test different types of model specification {{{
 # SFO_SFO {{{
 SFO_SFO.1 <- mkinmod(parent = list(type = "SFO", to = "m1"),
 		   m1 = list(type = "SFO"), use_of_ff = "min")
 SFO_SFO.2 <- mkinmod(parent = list(type = "SFO", to = "m1"),
 		   m1 = list(type = "SFO"), use_of_ff = "max")
-ot = seq(0, 100, by = 10)
+ot = seq(0, 100, by = 1)
 print(mkinpredict(SFO_SFO.1, c(k_parent_m1 = 0.1, k_parent_sink = 0.1, k_m1_sink = 0.1), 
-	    c(parent = 100, m1 = 0), ot, solution_type = "deSolve"))
+	    c(parent = 100, m1 = 0), ot, solution_type = "deSolve")[101,])
 print(mkinpredict(SFO_SFO.1, c(k_parent_m1 = 0.1, k_parent_sink = 0.1, k_m1_sink = 0.1), 
-	    c(parent = 100, m1 = 0), ot, solution_type = "eigen"))
+	    c(parent = 100, m1 = 0), ot, solution_type = "eigen")[101,])
 print(mkinpredict(SFO_SFO.2, c(k_parent = 0.2, f_parent_to_m1 = 0.5, k_m1 = 0.1), 
-	    c(parent = 100, m1 = 0), ot, solution_type = "deSolve"))
+	    c(parent = 100, m1 = 0), ot, solution_type = "deSolve")[101,])
 print(mkinpredict(SFO_SFO.2, c(k_parent = 0.2, f_parent_to_m1 = 0.5, k_m1 = 0.1), 
-	    c(parent = 100, m1 = 0), ot, solution_type = "eigen"))
+	    c(parent = 100, m1 = 0), ot, solution_type = "eigen")[101,])
 # }}}
 # SFO_SFO2_SFO {{{
 SFO_SFO2_SFO.1 <- mkinmod(parent = list(type = "SFO", to = c("m1", "m2")),
@@ -84,12 +69,8 @@ SFO_SFO2_SFO.2 <- mkinmod(parent = list(type = "SFO", to = c("m1", "m2")),
 		   m2 = list(type = "SFO"),
 		   m3 = list(type = "SFO"), 
 		   use_of_ff = "max")
-SFO_SFO2_SFO.2$diffs
+
 ot = seq(0, 100, by = 1)
-print(mkinpredict(SFO, c(k_parent_sink = 0.3),
-	    c(parent = 100), ot, solution_type = "deSolve"))
-print(mkinpredict(SFO, c(k_parent = 0.3),
-	    c(parent = 100), ot, solution_type = "analytical"))
 print(tail(mkinpredict(SFO_SFO2_SFO.1, c(k_parent_m1 = 0.1, k_parent_m2 = 0.1, 
 	    k_parent_sink = 0.1, k_m1_m3 = 0.1, k_m1_sink = 0.1, 
 	    k_m2_sink = 0.1, k_m3_sink = 0.1),
@@ -111,7 +92,7 @@ print(tail(mkinpredict(SFO_SFO2_SFO.2,
 	      k_m3 = 0.1),
 	    c(parent = 100, m1 = 0, m2 = 0, m3 = 0), ot, solution_type = "eigen")))
 # }}}
-# FOMC_SFO {{{
+# FOMC_SFO (actually two times the same model {{{
 FOMC_SFO.1 <- mkinmod(parent = list(type = "FOMC", to = "m1"),
 	           m1 = list(type = "SFO"))
 FOMC_SFO.2 <- mkinmod(parent = list(type = "FOMC", to = "m1"),
@@ -129,51 +110,112 @@ SFORB_SFO.1 <- mkinmod(parent = list(type = "SFORB", to = "m1"),
 SFORB_SFO.2 <- mkinmod(parent = list(type = "SFORB", to = "m1"),
 	           m1 = list(type = "SFO"), use_of_ff = "max")
 # }}}
-system.time(fit.SFORB <- mkinfit(SFORB_SFO, d2, 
-  parms.ini = f.SFORB$parms.all,
-  plot=TRUE))
-summary(fit.SFORB, data=FALSE)
+# }}}
 
-debug(mkinfit)
-debug(cost)
-undebug(mkinfit)
-d2 <- rbind(FOCUS_2006_C, 
-    data.frame(
-       name = "m1", 
-       time = FOCUS_2006_C$time,
-       value = c(0, 20, 40, 45, 48, 49, 48, 46, 45)))
-# Compare eigenvalue and deSolve based fitting for SFO_SFO {{{
+# Compare eigenvalue and deSolve based fitting {{{
+testdata = d2
+# SFO_SFO {{{
 # for both model specification variants
-system.time(fit.SFO.1.eigen <- mkinfit(SFO_SFO.1, d2, plot=TRUE))
-system.time(fit.SFO.1.lsoda <- mkinfit(SFO_SFO.1, d2, solution_type = "deSolve", plot=TRUE))
+system.time(fit.SFO.1.eigen <- mkinfit(SFO_SFO.1, testdata, plot=TRUE))
+system.time(fit.SFO.1.lsoda <- mkinfit(SFO_SFO.1, testdata, solution_type = "deSolve", plot=TRUE))
+system.time(fit.SFO.2.eigen <- mkinfit(SFO_SFO.2, testdata, plot=TRUE))
+f.SFO <- mkinfit(SFO.2, testdata)
+system.time(fit.SFO.2.eigen <- mkinfit(SFO_SFO.2, parms.ini = f.SFO$odeparms.final, 
+  testdata, plot=TRUE))
+system.time(fit.SFO.2.lsoda <- mkinfit(SFO_SFO.2, testdata, solution_type = "deSolve", plot=TRUE))
 summary(fit.SFO.1.eigen, data=FALSE)
 summary(fit.SFO.1.lsoda, data=FALSE)
-system.time(fit.SFO.2.eigen <- mkinfit(SFO_SFO.2, d2, plot=TRUE))
-system.time(fit.SFO.2.lsoda <- mkinfit(SFO_SFO.2, d2, solution_type = "deSolve", plot=TRUE))
 summary(fit.SFO.2.eigen, data=FALSE)
 summary(fit.SFO.2.lsoda, data=FALSE)
 # }}}
-system.time(fit.FOMC <- mkinfit(FOMC_SFO.1, d2, plot=TRUE))
-f.DFOP <- mkinfit(DFOP, d2)
-system.time(fit.DFOP <- mkinfit(DFOP_SFO, d2, 
-  parms.ini = f.DFOP$parms.all,
-  plot=TRUE))
-f.SFORB.1.eigen <- mkinfit(SFORB_SFO.1, d2, plot=TRUE)
-f.SFORB.1.lsoda <- mkinfit(SFORB_SFO.1, d2, solution_type = "deSolve", plot=TRUE)
+# SFORB_SFO {{{
+testdata = FOCUS_2006_D
+f.SFORB.1.eigen <- mkinfit(SFORB_SFO.1, testdata, plot=TRUE)
+f.SFORB.1.lsoda <- mkinfit(SFORB_SFO.1, testdata, solution_type = "deSolve", plot=TRUE)
+# SFORB_SFO.2 is not there because combining maximum use of ff with SFORB is not supported
 summary(f.SFORB.1.eigen, data = FALSE)
 summary(f.SFORB.1.lsoda, data = FALSE)
-system.time(fit.SFORB <- mkinfit(SFORB_SFO, d2, 
-  parms.ini = f.SFORB$parms.all,
-  plot=TRUE))
-summary(fit.SFORB, data=FALSE)
-system.time(fit.SFORB <- mkinfit(SFORB_SFO, d2, 
-  parms.ini = f.SFORB$parms.all,
-  eigen=TRUE,
-  plot=TRUE))
-summary(fit.SFORB, data=FALSE)
+# }}}
+# }}}
 
-system.time(fit.SFO <- mkinfit(SFO_SFO, FOCUS_2006_D, plot=TRUE))
-system.time(fit.SFORB <- mkinfit(SFORB_SFO, FOCUS_2006_D, plot=TRUE))
-summary(fit.SFO, data=FALSE)
+# Check coupled FOMC models {{{
+fit.FOMC <- mkinfit(FOMC, d2)
+system.time(fit.FOMC.1 <- mkinfit(FOMC_SFO.1, d2, plot=TRUE))
+system.time(fit.FOMC.1 <- mkinfit(FOMC_SFO.1, d2, parms.ini = fit.FOMC$odeparms.final, plot=TRUE))
+system.time(fit.FOMC.2 <- mkinfit(FOMC_SFO.2, d2, parms.ini = fit.FOMC$odeparms.final, plot=TRUE))
+summary(fit.FOMC, data=FALSE)
+summary(fit.FOMC.1, data=FALSE)
+summary(fit.FOMC.2, data=FALSE)
+fit.FOMC$distimes
+fit.FOMC.1$distimes
+fit.FOMC.2$distimes
+# }}}
+
+# Check DFOP_SFO {{{
+DFOP_SFO <- mkinmod(parent = list(type = "DFOP", to = "m1"),
+	           m1 = list(type = "SFO"))
+f.DFOP <- mkinfit(DFOP, d2)
+summary(f.DFOP)
+system.time(fit.DFOP <- mkinfit(DFOP_SFO, d2, 
+  plot=TRUE))
+system.time(fit.DFOP <- mkinfit(DFOP_SFO, FOCUS_2006_D, 
+  plot=TRUE))
+summary(fit.DFOP, data=FALSE)
+# }}}
+
+# {{{ KinGUI test data from 2007
+data <- mkin_wide_to_long(schaefer07_complex_case, time = "time")
+debug(mkinmod)
+model.1 <- mkinmod(
+    parent = list(type = "SFO", to = c("A1", "B1", "C1"), sink = FALSE),
+    A1 = list(type = "SFO", to = "A2"),
+    B1 = list(type = "SFO"),
+    C1 = list(type = "SFO"),
+    A2 = list(type = "SFO"))
+model.1.sink <- mkinmod(
+    parent = list(type = "SFO", to = c("A1", "B1", "C1"), sink = TRUE),
+    A1 = list(type = "SFO", to = "A2"),
+    B1 = list(type = "SFO"),
+    C1 = list(type = "SFO"),
+    A2 = list(type = "SFO"))
+model.2 <- mkinmod(
+    parent = list(type = "SFO", to = c("A1", "B1", "C1"), sink = FALSE),
+    A1 = list(type = "SFO", to = "A2"),
+    B1 = list(type = "SFO"),
+    C1 = list(type = "SFO"),
+    A2 = list(type = "SFO"), use_of_ff = "max")
+
+# Compare eigenvalue and deSolve based solutions {{{
+undebug(mkinpredict)
+print(mkinpredict(model.1, 
+  fit.1.eigen$odeparms.final, 
+  c(parent = 100, A1 = 0, B1 = 0, C1 = 0, A2 = 0),
+  outtimes = 0:100, solution_type = "eigen")[101,])
+print(mkinpredict(model.1, 
+  fit.1.eigen$odeparms.final, 
+  c(parent = 100, A1 = 0, B1 = 0, C1 = 0, A2 = 0),
+  outtimes = 0:100, solution_type = "deSolve")[101,])
+# }}}
+
+fit.1.eigen <- mkinfit(model.1, data, plot=TRUE)
+fit.1.lsoda <- mkinfit(model.1, data, solution_type = "deSolve", plot=TRUE)
+fit.1.lsoda <- mkinfit(model.1, data, solution_type = "deSolve", plot=TRUE)
+fit.1.lsoda.1000 <- mkinfit(model.1, data, solution_type = "deSolve", n.outtimes = 1000, plot=TRUE)
+fit.1.eigen.sink <- mkinfit(model.1.sink, data, plot=TRUE)
+#fit.2.eigen <- mkinfit(model.2, data, plot=TRUE) # Does not work, singular matrix...
+fit.2.lsoda <- mkinfit(model.2, data, solution_type = "deSolve", plot=TRUE)
+fit.2.lsoda.1000 <- mkinfit(model.2, data, solution_type = "deSolve", n.outtimes = 1000, plot=TRUE)
+summary(fit.1.eigen, data=FALSE) # Fast and fits very nicely with Schaefer 2007
+summary(fit.1.lsoda, data=FALSE) # Slower, not precise, presumably because the model does not fit the data well (no sink term...), but maybe also a different ode solver would do...
+summary(fit.1.lsoda.1000, data=FALSE) # Gives solution much closer to eigen solution, but slow!
+summary(fit.1.eigen.sink, data=FALSE)
+#summary(fit.2.eigen, data=FALSE) # See above, did not work
+summary(fit.2.lsoda, data=FALSE)
+summary(fit.2.lsoda.1000, data=FALSE) # Not a lot different from n.outtimes = 100
+
+# Compare the models with sink - the solution method does not make a significant difference
+fit.1.eigen.sink$distimes
+fit.2.lsoda$distimes
+# }}}
 
 # vim: set foldmethod=marker:
