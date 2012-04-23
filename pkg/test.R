51717ded9a55fbe6d0c56aacd513d452a1b8d033
jranke (Johannes Ranke) <jranke@uni-bremen.de>
2012-04-23 06:36:24 +0000

    - Some fixes to mkinfit, to account for the changes in mkinmod - See TODO for what remains to be done (not complete)
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/kinfit@29 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/test.R
+++ pkg/test.R
@@ -1,4 +1,3 @@
-install.packages("mkin")
 library(colorout)
 library(mkin)
 # {{{ Source new versions of mkin functions
@@ -53,47 +52,116 @@ summary(fit3)#}}}
 
 debug(mkinmod)
 
+test <- mkinmod(parent = list(type = "SFO", 
+
 SFO <- mkinmod(parent = list(type = "SFO"))
 DFOP <- mkinmod(parent = list(type = "DFOP"))
 SFORB <- mkinmod(parent = list(type = "SFORB"))
-SFO_SFO <- mkinmod(parent = list(type = "SFO", to = "m1"),
+# Test different types of model specification and solution
+# SFO_SFO {{{
+SFO_SFO.1 <- mkinmod(parent = list(type = "SFO", to = "m1"),
+		   m1 = list(type = "SFO"), use_of_ff = "min")
+SFO_SFO.2 <- mkinmod(parent = list(type = "SFO", to = "m1"),
+		   m1 = list(type = "SFO"), use_of_ff = "max")
+ot = seq(0, 100, by = 10)
+print(mkinpredict(SFO_SFO.1, c(k_parent_m1 = 0.1, k_parent_sink = 0.1, k_m1_sink = 0.1), 
+	    c(parent = 100, m1 = 0), ot, solution_type = "deSolve"))
+print(mkinpredict(SFO_SFO.1, c(k_parent_m1 = 0.1, k_parent_sink = 0.1, k_m1_sink = 0.1), 
+	    c(parent = 100, m1 = 0), ot, solution_type = "eigen"))
+print(mkinpredict(SFO_SFO.2, c(k_parent = 0.2, f_parent_to_m1 = 0.5, k_m1 = 0.1), 
+	    c(parent = 100, m1 = 0), ot, solution_type = "deSolve"))
+print(mkinpredict(SFO_SFO.2, c(k_parent = 0.2, f_parent_to_m1 = 0.5, k_m1 = 0.1), 
+	    c(parent = 100, m1 = 0), ot, solution_type = "eigen"))
+# }}}
+# SFO_SFO2_SFO {{{
+SFO_SFO2_SFO.1 <- mkinmod(parent = list(type = "SFO", to = c("m1", "m2")),
+		   m1 = list(type = "SFO", to = "m3"),
+		   m2 = list(type = "SFO"),
+		   m3 = list(type = "SFO"), 
+		   use_of_ff = "min")
+SFO_SFO2_SFO.2 <- mkinmod(parent = list(type = "SFO", to = c("m1", "m2")),
+		   m1 = list(type = "SFO", to = "m3"),
+		   m2 = list(type = "SFO"),
+		   m3 = list(type = "SFO"), 
+		   use_of_ff = "max")
+SFO_SFO2_SFO.2$diffs
+ot = seq(0, 100, by = 1)
+print(mkinpredict(SFO, c(k_parent_sink = 0.3),
+	    c(parent = 100), ot, solution_type = "deSolve"))
+print(mkinpredict(SFO, c(k_parent = 0.3),
+	    c(parent = 100), ot, solution_type = "analytical"))
+print(tail(mkinpredict(SFO_SFO2_SFO.1, c(k_parent_m1 = 0.1, k_parent_m2 = 0.1, 
+	    k_parent_sink = 0.1, k_m1_m3 = 0.1, k_m1_sink = 0.1, 
+	    k_m2_sink = 0.1, k_m3_sink = 0.1),
+	    c(parent = 100, m1 = 0, m2 = 0, m3 = 0), ot, solution_type = "deSolve")))
+print(tail(mkinpredict(SFO_SFO2_SFO.1, c(k_parent_m1 = 0.1, k_parent_m2 = 0.1, 
+	    k_parent_sink = 0.1, k_m1_m3 = 0.1, k_m1_sink = 0.1, 
+	    k_m2_sink = 0.1, k_m3_sink = 0.1),
+	    c(parent = 100, m1 = 0, m2 = 0, m3 = 0), ot, solution_type = "eigen")))
+print(tail(mkinpredict(SFO_SFO2_SFO.2, 
+            c(k_parent = 0.3, f_parent_to_m1 = 1/3, f_parent_to_m2 = 1/3, 
+              k_m1 = 0.2, f_m1_to_m3 = 0.5,
+	      k_m2 = 0.1,
+	      k_m3 = 0.1),
+	    c(parent = 100, m1 = 0, m2 = 0, m3 = 0), ot, solution_type = "deSolve")))
+print(tail(mkinpredict(SFO_SFO2_SFO.2, 
+            c(k_parent = 0.3, f_parent_to_m1 = 1/3, f_parent_to_m2 = 1/3, 
+              k_m1 = 0.2, f_m1_to_m3 = 0.5,
+	      k_m2 = 0.1,
+	      k_m3 = 0.1),
+	    c(parent = 100, m1 = 0, m2 = 0, m3 = 0), ot, solution_type = "eigen")))
+# }}}
+# FOMC_SFO {{{
+FOMC_SFO.1 <- mkinmod(parent = list(type = "FOMC", to = "m1"),
 	           m1 = list(type = "SFO"))
-SFO_SFO <- mkinmod(parent = list(type = "SFO", to = "m1"),
+FOMC_SFO.2 <- mkinmod(parent = list(type = "FOMC", to = "m1"),
 	           m1 = list(type = "SFO"), use_of_ff = "max")
-FOMC_SFO <- mkinmod(parent = list(type = "FOMC", to = "m1"),
+print(tail(mkinpredict(FOMC_SFO.1,
+  c(alpha = 1, beta = 10, f_parent_to_m1 = 0.5, k_m1_sink = 0.1),
+  c(parent = 100, m1 = 0), ot, solution_type = "deSolve")))
+print(tail(mkinpredict(FOMC_SFO.1,
+  c(alpha = 1, beta = 10, f_parent_to_m1 = 0.5, k_m1_sink = 0.1),
+  c(parent = 100, m1 = 0), ot, solution_type = "deSolve")))
+# }}}
+# SFORB_SFO {{{
+SFORB_SFO.1 <- mkinmod(parent = list(type = "SFORB", to = "m1"),
 	           m1 = list(type = "SFO"))
-FOMC_SFO <- mkinmod(parent = list(type = "FOMC", to = "m1"),
+SFORB_SFO.2 <- mkinmod(parent = list(type = "SFORB", to = "m1"),
 	           m1 = list(type = "SFO"), use_of_ff = "max")
-DFOP_SFO <- mkinmod(parent = list(type = "DFOP", to = "m1"),
-	           m1 = list(type = "SFO"))
-DFOP_SFO <- mkinmod(parent = list(type = "DFOP", to = "m1"),
-	           m1 = list(type = "SFO"), use_of_ff = "max")
-SFORB_SFO <- mkinmod(parent = list(type = "SFORB", to = "m1"),
-	           m1 = list(type = "SFO"))
-SFORB_SFO <- mkinmod(parent = list(type = "SFORB", to = "m1"),
-	           m1 = list(type = "SFO"), use_of_ff = "max")
-SFORB_SFO
+# }}}
 system.time(fit.SFORB <- mkinfit(SFORB_SFO, d2, 
   parms.ini = f.SFORB$parms.all,
   plot=TRUE))
 summary(fit.SFORB, data=FALSE)
 
-debug(transform_odeparms)
-undebug(transform_odeparms)
+debug(mkinfit)
+debug(cost)
+undebug(mkinfit)
 d2 <- rbind(FOCUS_2006_C, 
     data.frame(
        name = "m1", 
        time = FOCUS_2006_C$time,
        value = c(0, 20, 40, 45, 48, 49, 48, 46, 45)))
-system.time(fit.SFO <- mkinfit(SFO_SFO, d2, plot=TRUE))
-summary(fit.SFO)
-system.time(fit.FOMC <- mkinfit(FOMC_SFO, d2, plot=TRUE))
+# Compare eigenvalue and deSolve based fitting for SFO_SFO {{{
+# for both model specification variants
+system.time(fit.SFO.1.eigen <- mkinfit(SFO_SFO.1, d2, plot=TRUE))
+system.time(fit.SFO.1.lsoda <- mkinfit(SFO_SFO.1, d2, solution_type = "deSolve", plot=TRUE))
+summary(fit.SFO.1.eigen, data=FALSE)
+summary(fit.SFO.1.lsoda, data=FALSE)
+system.time(fit.SFO.2.eigen <- mkinfit(SFO_SFO.2, d2, plot=TRUE))
+system.time(fit.SFO.2.lsoda <- mkinfit(SFO_SFO.2, d2, solution_type = "deSolve", plot=TRUE))
+summary(fit.SFO.2.eigen, data=FALSE)
+summary(fit.SFO.2.lsoda, data=FALSE)
+# }}}
+system.time(fit.FOMC <- mkinfit(FOMC_SFO.1, d2, plot=TRUE))
 f.DFOP <- mkinfit(DFOP, d2)
 system.time(fit.DFOP <- mkinfit(DFOP_SFO, d2, 
   parms.ini = f.DFOP$parms.all,
   plot=TRUE))
-f.SFORB <- mkinfit(SFORB, d2, plot=TRUE)
-summary(f.SFORB, data = FALSE)
+f.SFORB.1.eigen <- mkinfit(SFORB_SFO.1, d2, plot=TRUE)
+f.SFORB.1.lsoda <- mkinfit(SFORB_SFO.1, d2, solution_type = "deSolve", plot=TRUE)
+summary(f.SFORB.1.eigen, data = FALSE)
+summary(f.SFORB.1.lsoda, data = FALSE)
 system.time(fit.SFORB <- mkinfit(SFORB_SFO, d2, 
   parms.ini = f.SFORB$parms.all,
   plot=TRUE))
