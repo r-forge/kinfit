9bebae0e4fb99f26dbd08631e10c6e761dab7f42
jranke (Johannes Ranke) <jranke@uni-bremen.de>
2016-06-27 23:46:34 +0000

    sync with git
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/kinfit@304 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/mkin/R/plot.mmkin.R
+++ pkg/mkin/R/plot.mmkin.R
@@ -1,8 +1,5 @@
-# Copyright (C) 2015 Johannes Ranke
+# Copyright (C) 2015-2016 Johannes Ranke
 # Contact: jranke@uni-bremen.de
-# The summary function is an adapted and extended version of summary.modFit
-# from the FME package, v 1.1 by Soetart and Petzoldt, which was in turn
-# inspired by summary.nls.lm
 
 # This file is part of the R package mkin
 
@@ -19,16 +16,21 @@
 # You should have received a copy of the GNU General Public License along with
 # this program. If not, see <http://www.gnu.org/licenses/>
 
-plot.mmkin <- function(x, main = "auto", legends = 1, errmin_var = "All data", errmin_digits = 2, 
+plot.mmkin <- function(x, main = "auto", legends = 1, errmin_var = "All data", errmin_digits = 3, 
                        cex = 0.7, rel.height.middle = 0.9, ...) {
   n.m <- nrow(x)
   n.d <- ncol(x)
+
+  # We can handle either a row (different models, same dataset) 
+  # or a column (same model, different datasets)
   if (n.m > 1 & n.d > 1) stop("Please select fits either for one model or for one dataset")
   if (n.m == 1 & n.d == 1) loop_over = "none"
   if (n.m > 1) loop_over <- "models"
   if (n.d > 1) loop_over <- "datasets"
   n.fits <- length(x)
 
+  # Set the main plot titles from the names of the models or the datasets
+  # Will be integer indexes if no other names are present in the mmkin object
   if (main == "auto") {
     main = switch(loop_over,
                   none = paste(rownames(x), colnames(x)),
@@ -37,26 +39,36 @@ plot.mmkin <- function(x, main = "auto", legends = 1, errmin_var = "All data", e
   }
 
   oldpar <- par(no.readonly = TRUE)
+
+  # Set relative plot heights, so the first and the last plot are the norm
+  # and the middle plots (if n.fits >2) are smaller by rel.height.middle
   rel.heights <- if (n.fits > 2) c(1, rep(rel.height.middle, n.fits - 2), 1)
                  else rep(1, n.fits)
   layout(matrix(1:(2 * n.fits), n.fits, 2, byrow = TRUE), heights = rel.heights)
 
-  #par(mfrow = c(n.fits, 2))
-  par(mar = c(3.0, 4.1, 4.1, 2.1)) # Reduce bottom margin by 2.1 - hides x axis legend
   par(cex = cex)
 
   for (i.fit in 1:n.fits) {
-    if (i.fit == 2) {
+
+    # Margins for top row of plots when we have more than one row
+    # Reduce bottom margin by 2.1 - hides x axis legend
+    if (i.fit == 1 & n.fits > 1) {
+      par(mar = c(3.0, 4.1, 4.1, 2.1)) 
+    }
+
+    # Margins for middle rows of plots, if any
+    if (i.fit > 1 & i.fit < n.fits) {
       # Reduce top margin by 2 after the first plot as we have no main title, 
       # reduced plot height, therefore we need rel.height.middle in the layout
       par(mar = c(3.0, 4.1, 2.1, 2.1))
     }
-    if (i.fit == n.fits) {
-      # Reduce top margin by 2 after the first plot as we have no main title, 
-      # plot height remains about constant
-      par(mar = c(5.1, 4.1, 2.1, 2.1))
 
+    # Margins for bottom row of plots when we have more than one row
+    if (i.fit == n.fits & n.fits > 1) {
+      # Restore bottom margin for last plot to show x axis legend
+      par(mar = c(5.1, 4.1, 2.1, 2.1))
     }
+
     fit <- x[[i.fit]]
     plot(fit, legend = legends == i.fit, ...)
 
@@ -67,10 +79,9 @@ plot.mmkin <- function(x, main = "auto", legends = 1, errmin_var = "All data", e
                        datasets = colnames(x)[i.fit],
                        none = "") 
 
-    if (!is.null(errmin_var)) {
-      chi2 <- paste0(round(100 * mkinerrmin(fit)[errmin_var, "err.min"], errmin_digits), "%")
-      mtext(bquote(.(fit_name) ~ chi^2 ~ "error level" == .(chi2)), cex = cex, line = 0.4)
-    }
+    chi2 <- paste0(signif(100 * mkinerrmin(fit)[errmin_var, "err.min"], errmin_digits), "%")
+    mtext(bquote(.(fit_name) ~ chi^2 ~ "error level" == .(chi2)), cex = cex, line = 0.4)
+
     mkinresplot(fit, legend = FALSE, ...)
     mtext(paste(fit_name, "residuals"), cex = cex, line = 0.4)
   }
